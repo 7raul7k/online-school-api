bf15d23e9405b4761a41f86d6ccbce8198c50597
7raul7k <balanescuraul@gmail.com>
2023-09-17 14:48:14 +0300

    add main

---
+++ src/test/java/ro/myclass/onlineschoolapi/OnlineSchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.myclass.onlineschoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
