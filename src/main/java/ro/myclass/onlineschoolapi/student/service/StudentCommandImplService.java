58480533ab310b86b2bf2932c5276fbb25cfad96
7raul7k <balanescuraul@gmail.com>
2023-09-18 18:17:40 +0300

    add student-command-service

--- src/main/java/ro/myclass/onlineschoolapi/student/service/StudentCommandImplService.java
+++ src/main/java/ro/myclass/onlineschoolapi/student/service/StudentCommandImplService.java
@@ -1,2 +1,74 @@
-package ro.myclass.onlineschoolapi.student.service;public class StudentCommandImplService {
+package ro.myclass.onlineschoolapi.student.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import ro.myclass.onlineschoolapi.exceptions.StudentNotFoundException;
+import ro.myclass.onlineschoolapi.exceptions.StudentWasFoundException;
+import ro.myclass.onlineschoolapi.student.dto.StudentDTO;
+import ro.myclass.onlineschoolapi.student.model.Student;
+import ro.myclass.onlineschoolapi.student.repo.StudentRepo;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class StudentCommandImplService implements StudentCommandService {
+
+    private StudentRepo studentRepo;
+
+    public StudentCommandImplService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    public void addStudent(StudentDTO studentDTO) {
+
+        Optional<Student> student = studentRepo.findStudentByFirstNameAndLastName(studentDTO.getFirstName(), studentDTO.getLastName());
+
+        if(student.isEmpty()){
+            Student student1 = Student.builder().firstName(studentDTO.getFirstName()).lastName(studentDTO.getLastName()).age(studentDTO.getAge()).email(studentDTO.getEmail()).adress(studentDTO.getAdress()).build();
+
+            studentRepo.save(student1);
+        }
+
+        throw new StudentWasFoundException();
+
+    }
+
+    public void deleteStudent(String firstName, String lastName) {
+
+        Optional<Student> student = studentRepo.findStudentByFirstNameAndLastName(firstName, lastName);
+
+      if(student.isEmpty()){
+            throw new StudentNotFoundException();
+      }else{
+            studentRepo.delete(student.get());
+      }
+
+    }
+
+    public void updateStudent(StudentDTO studentDTO){
+
+        Optional<Student> student = studentRepo.findStudentByEmail(studentDTO.getEmail());
+        if(student.isEmpty()) {
+            throw new StudentNotFoundException();
+        }else {
+
+            if (studentDTO.getFirstName() != null) {
+                student.get().setFirstName(studentDTO.getFirstName());
+            }
+            if (studentDTO.getLastName() != null) {
+                student.get().setLastName(studentDTO.getLastName());
+            }if (studentDTO.getAge() != 0) {
+                student.get().setAge(studentDTO.getAge());
+            }if(studentDTO.getEmail() != null){
+                student.get().setEmail(studentDTO.getEmail());
+            }
+
+            this.studentRepo.saveAndFlush(student.get());
+            }
+     }
+
+
+
+
 }
--- src/main/java/ro/myclass/onlineschoolapi/student/service/StudentCommandService.java
+++ src/main/java/ro/myclass/onlineschoolapi/student/service/StudentCommandService.java
@@ -1,2 +1,16 @@
-package ro.myclass.onlineschoolapi.student.service;public interface StudentCommandService {
+package ro.myclass.onlineschoolapi.student.service;
+
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import ro.myclass.onlineschoolapi.student.dto.StudentDTO;
+
+@Service
+@Transactional
+public interface StudentCommandService {
+
+    void addStudent(StudentDTO studentDTO);
+    void deleteStudent(String firstName, String lastName);
+
+    void updateStudent(StudentDTO studentDTO);
 }
